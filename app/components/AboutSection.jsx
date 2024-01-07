03a5e80a953e992589a9ae820a0e491e731728ae
Naman Chauhan <chauhannaman98@gmail.com>
2024-01-07 23:41:41 +0530

    Minor UI changes

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -54,9 +54,9 @@ const AboutSection = () => {
 
     return (
         <section>
-            <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+            <div className='mt-10 md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
                 <Image src="/images/about-image1.png" width={500} height={500} alt='about-image'/>
-                <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
+                <div className='mt-6 md:mt-0 text-left flex flex-col h-full'>
                     <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
                     <p className='text-base lg:text-lg'>
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium laboriosam consequatur itaque, 
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -10,7 +10,7 @@ export const HeroSection = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+                    <span className="text-5xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
                         Hello, I'm {""}
                     </span>
                     <br />
