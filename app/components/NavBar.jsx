7bfccf42aadd4defae76386b8f282e03c23225ab
Naman Chauhan <chauhannaman98@gmail.com>
2024-01-07 21:15:48 +0530

    Improved menufor mobile

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,6 +1,9 @@
+"use client";
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import NavLink from './NavLink';
+import { Bars3Icon, XMarkIcon, XmarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from './MenuOverlay';
 
 const navLinks = [
   {
@@ -18,10 +21,34 @@ const navLinks = [
 ];
 
 export const NavBar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
-    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
         <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-            <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>LOGO</Link>
+            <Link 
+              href={"/"} 
+              className='text-3xl md:text-4xl text-white font-semibold'
+            >
+              LOGO
+            </Link>
+            <div className="mobile-menu block md:hidden">
+            {!navbarOpen ? (
+              <button
+                onClick={() => setNavbarOpen(true)}
+                className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              >
+                <Bars3Icon className="h-5 w-5" />
+              </button>
+              ) : (
+              <button
+                onClick={() => setNavbarOpen(false)}
+                className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              >
+                <XMarkIcon className="h-5 w-5" />
+              </button>
+            )}
+            </div>
             <div className='menu hidden md:block md:w-auto' id='navbar'>
               <ul className='flex justify-evenly p-4 md:p-0 md:flex-row md:space-x-8'>
                 {
@@ -34,6 +61,9 @@ export const NavBar = () => {
               </ul>
             </div>
         </div>
+        {
+          navbarOpen ? <MenuOverlay links={navLinks}/> : null
+        }
     </nav>
   );
 };
--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
     <NavBar />
-      <div className="container mt-24 mx-auto px-12 py-12">
+      <div className="container mt-32 mx-auto px-12 py-12">
         <HeroSection />
       </div>
     </main>
