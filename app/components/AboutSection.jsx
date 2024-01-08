1db0ca03dd50891c9b6964e0c4b13ca26b9a8b8a
Naman Chauhan <chauhannaman98@gmail.com>
2024-01-08 22:04:30 +0530

    Rounded image & slowed animation

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -55,7 +55,7 @@ const AboutSection = () => {
     return (
         <section>
             <div className='mt-10 md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-                <Image src="/images/about-image1.png" width={500} height={500} alt='about-image'/>
+                <Image className='rounded-2xl' src="/images/about-image1.png" width={500} height={500} alt='about-image'/>
                 <div className='mt-6 md:mt-0 text-left flex flex-col h-full'>
                     <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
                     <p className='text-base lg:text-lg'>
