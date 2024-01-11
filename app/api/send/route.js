81cb14f2f079810d4f6f5e27d29da40e5cd987bc
Naman Chauhan <chauhannaman98@gmail.com>
2024-01-11 12:51:19 +0530

    Added email section

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -4,21 +4,27 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: ['chauhannaman98@gmail.com'],
-      subject: 'Hello world',
-      react: (
-        <>
-            <p>Email Body</p>
-        </>
-        ),
-    });
+export async function POST(req, res) {
+    const { body } = req;
+    const { email, subject, message } = body;
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
+    try {
+        const data = await resend.emails.send({
+        from: fromEmail,
+        to: ['chauhannaman98@gmail.com', email],
+        subject: subject,
+        react: (
+            <>
+                <h1>{subject}</h1>
+                <p>Thank you for contacting us!</p>
+                <p>New message submmitted.</p>
+                <p>{message}</p>
+            </>
+            ),
+        });
+
+        return Response.json(data);
+    } catch (error) {
+        return Response.json({ error });
+    }
 }
