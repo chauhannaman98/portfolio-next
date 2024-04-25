5fc51b69380a901837c9369a5bb3e8ba02dcd62e
Naman Chauhan <chauhannaman98@gmail.com>
2024-04-25 18:14:22 +0530

    Added new project

---
+++ app/components/AboutWebsite.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutWebsite = () => {
+  return (
+    <section id='about-website' className='mt-16'>
+        <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>How this website was created?</h2>
+    </section>
+  )
+};
+
+export default AboutWebsite;
